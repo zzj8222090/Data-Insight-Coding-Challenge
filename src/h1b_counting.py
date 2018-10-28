a99fd980dcc842b92627390de2f5939c1721bcc5
zijianz <zijianz@iastate.edu>
2018-10-28 16:03:14 -0500

    modified writing to files

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -29,14 +29,14 @@ def top_certified(name):
     occupation = [record[occupation_col] for record in splitdata if record[status_col] == 'CERTIFIED']
     states = [record[state_col] for record in splitdata if record[status_col] == 'CERTIFIED']
 
-    # Count the top 10 occupations, find the names, numbers and percentage, Counter returns a list of tuples.
+    # Count the top 10 occupations, sort by values then by keys, find the names, numbers and percentage, Counter returns a list of tuples.
     count_occ = dict(Counter(occupation).most_common(10))
     count_occ = sorted(count_occ.items(), key=lambda x: (-x[1], x[0]))
     top_occ = [i[0] for i in count_occ]
     top_occ_num = [i[1] for i in count_occ]
     percentage_occ = [round(i / len(occupation) * 100, 1) for i in top_occ_num]
 
-    # Count the top 10 states, find the names, numbers and percentage
+    # Count the top 10 states, sort by values then by keys, find the names, numbers and percentage
     count_state = dict(Counter(states).most_common(10))
     count_state = sorted(count_state.items(), key=lambda x: (-x[1], x[0]))
     top_state = [i[0] for i in count_state]
@@ -49,8 +49,9 @@ def top_certified(name):
     state_output = open(sys.argv[3], 'w')
     state_output.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
     # Write results to files
-    for i in range(10):
+    for i in range(len(top_occ)):
         occ_output.write(str(top_occ[i]) + ';' + str(top_occ_num[i]) + ';' + str(percentage_occ[i]) + '%\n')
+    for i in range(len(top_state)):
         state_output.write(str(top_state[i]) + ';' + str(top_state_num[i]) + ';' + str(percentage_state[i]) + '%\n')
 
 
