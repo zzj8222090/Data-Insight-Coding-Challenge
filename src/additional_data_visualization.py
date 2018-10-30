ffc6a06cff5a8a781cad187e3611f01961100f0a
Zijian Zhao <zhaozijian1219@gmail.com>
2018-10-30 00:31:24 -0500

    add plot

--- src/additional_data_visualization.py
+++ src/additional_data_visualization.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+#return the top occupations
 def getocc(name):
     f = pd.read_csv(name, sep=';', index_col=0, header=0)
     labels = list(f.columns)
@@ -20,6 +21,7 @@ def getocc(name):
     return top_name, top_occ_num
 
 
+#return the top states
 def getstate(name):
     f = pd.read_csv(name, sep=';', index_col=0, header=0)
     labels = list(f.columns)
@@ -39,6 +41,7 @@ def getstate(name):
     return top_name, top_state_num
 
 
+#plot pie plot for each occupation/state
 def pieplot(name, flag):
     if flag == 1:
         top_name, top_num = getocc(name)
@@ -56,7 +59,7 @@ def pieplot(name, flag):
 
 # pieplot('H1B_FY_2016.csv',2)
 
-
+#plot the number of certified and non-certified VISAs for each year
 def ifcerbars(name):
     x = [2014, 2015, 2016]
     num_cer = []
@@ -83,12 +86,60 @@ def ifcerbars(name):
     plt.show()
 
 
-ifcerbars('H1B_FY_')
+# ifcerbars('H1B_FY_')
 
 
-def certifiedtype(name):
-    f = pd.read_csv(name +'.csv', sep=';', index_col=0, header=0)
-    print(f.columns)
-    print(f.groupby('VISA_CLASS').count().sort_values('STATUS', ascending=False)['STATUS'])
+#plot the approved working start quarters for each year
+def plotquarters(name):
+    x=[2014,2015,2016]
+    years = []
+    for i in x:
+        print(i)
+        f = pd.read_csv(name +str(i)+'.csv', sep=';', index_col=0, header=0)
+        if i==2014:
+            certified = f.loc[f['STATUS'] == 'CERTIFIED']
+            date = "LCA_CASE_EMPLOYMENT_START_DATE"
+            certified["Q"] = 0
+            certified["L"] = certified[date].apply(lambda x: len(str(x).split("-")))
+            certified = certified.loc[certified["L"] != 1]
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["01", "02", "03"]), "Q"] = 1
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["04", "05", "06"]), "Q"] = 2
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["07", "08", "09"]), "Q"] = 3
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["10", "11", "12"]), "Q"] = 4
+            quarters = list(certified.groupby('Q').count()['LCA_CASE_EMPLOYMENT_START_DATE'])
+        elif i==2015:
+            certified = f.loc[f['CASE_STATUS'] == 'CERTIFIED']
+            date = "EMPLOYMENT_START_DATE"
+            certified["Q"] = 0
+            certified["L"] = certified[date].apply(lambda x: len(str(x).split("/")))
+            certified = certified.loc[certified["L"] ==3]
+            certified.loc[certified[date].apply(lambda x: x.split("/")[0]).isin(["01", "02", "03"]), "Q"] = 1
+            certified.loc[certified[date].apply(lambda x: x.split("/")[0]).isin(["04", "05", "06"]), "Q"] = 2
+            certified.loc[certified[date].apply(lambda x: x.split("/")[0]).isin(["07", "08", "09"]), "Q"] = 3
+            certified.loc[certified[date].apply(lambda x: x.split("/")[0]).isin(["10", "11", "12"]), "Q"] = 4
+            quarters = list(certified.groupby('Q').count()['EMPLOYMENT_START_DATE'])[1:]
+        else:
+            certified = f.loc[f['CASE_STATUS'] == 'CERTIFIED']
+            date = "EMPLOYMENT_START_DATE"
+            certified["Q"] = 0
+            certified["L"] = certified[date].apply(lambda x: len(str(x).split("-")))
+            certified = certified.loc[certified["L"] != 1]
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["01", "02", "03"]), "Q"] = 1
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["04", "05", "06"]), "Q"] = 2
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["07", "08", "09"]), "Q"] = 3
+            certified.loc[certified[date].apply(lambda x: x.split("-")[1]).isin(["10", "11", "12"]), "Q"] = 4
+            quarters = list(certified.groupby('Q').count()['EMPLOYMENT_START_DATE'])
+        print(quarters)
+        years.append(quarters)
+    ax = plt.axes()
+    mark = ['+','.','x']
+    for i in range(3):
+        ax.plot([1,2,3,4],years[i],mark[i],markersize=10)
+    ax.set_title('Approved Working Start Quarter of Years')
+    ax.set_xticks((1,2,3,4))
+    ax.set_xticklabels(['Quarter I','Quarter II','Quarter III','Quarter IV'])
+    ax.legend(('2014','2015','2016'))
+    plt.show()
 
-# certifiedtype('H1B_FY_2014')
+
+# plotquarters('H1B_FY_')
