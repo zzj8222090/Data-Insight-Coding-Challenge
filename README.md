4e3974d1b81584e04b2a653d41ff2b5ac5946bd9
Zijian Zhao <zhaozijian1219@gmail.com>
2018-10-29 22:22:43 -0500

    add plot

--- README.md
+++ README.md
@@ -99,7 +99,7 @@ ACCOUNTANTS AND AUDITORS;10187;1.8%
 NETWORK AND COMPUTER SYSTEMS ADMINISTRATORS;9876;1.7%  
 MECHANICAL ENGINEERS;8315;1.5%  
 ```
-<img src="/src/plots/2016_occ_pie.png" width="680" height="500">
+<img src="/src/plots/2016_occ_pie.png" width="680" height="480">
 
 ```
 TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE  
@@ -115,6 +115,11 @@ WA;20387;3.6%
 FL;18684;3.3%  
 ```
 <img src="/src/plots/2014_state_pie.png" width="520" height="480">
+
+## **Additional Analysis**
+
+<img src="/src/plots/bars.png" width="680" height="480">
+
 
 ## **Comments**
 
--- src/additional_data_visualization.py
+++ src/additional_data_visualization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -49,5 +50,31 @@ def pieplot(name,flag):
     plt.legend(top_name, loc=1)
     plt.show()
 
-pieplot('H1B_FY_2016.csv',2)
-# getdata('H1B_FY_2014.csv')
+# pieplot('H1B_FY_2016.csv',2)
+
+
+def getdata(name):
+    x = [2014,2015,2016]
+    num_cer = []
+    num_noncer = []
+    for i in x:
+        f = pd.read_csv(name + str(i) + '.csv',sep=';',index_col=0,header=0)
+        if i ==2014:
+            data = len(f.loc[f['STATUS'] == 'CERTIFIED'])
+        else:
+            data = len(f.loc[f['CASE_STATUS'] == 'CERTIFIED'])
+
+        num_cer.append(data)
+        num_noncer.append(len(f)-data)
+    ind = np.arange(3)
+    fig,ax = plt.subplots()
+    ax.bar(ind,num_cer,width=0.2,color='lightskyblue')
+    ax.bar(ind+0.2,num_noncer,width=0.2,color='yellowgreen')
+    ax.legend(('CERTIFIED','NON-CERTIFIED'))
+    ax.set_xticks(ind+0.1)
+    ax.set_ylim((0,600000))
+    ax.set_xticklabels(('2014','2015','2016'))
+
+    plt.show()
+
+getdata('H1B_FY_')
