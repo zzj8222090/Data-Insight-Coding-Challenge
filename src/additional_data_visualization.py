ecb3c473a6d68902b3f27c0983014c819332110f
Zijian Zhao <zhaozijian1219@gmail.com>
2018-10-29 21:11:32 -0500

    add plot

--- src/additional_data_visualization.py
+++ src/additional_data_visualization.py
@@ -5,10 +5,15 @@ from matplotlib import pyplot as plt
 def getocc(name):
     f = pd.read_csv(name,sep=';',index_col=0,header=0)
     labels = list(f.columns)
-    top = f.loc[f['STATUS']=='CERTIFIED'].groupby('LCA_CASE_SOC_NAME').count().sort_values('STATUS',ascending=False)['STATUS'][0:10]
+    if '14' in name:
+        certified = f.loc[f['STATUS']=='CERTIFIED']
+        top = certified.groupby('LCA_CASE_SOC_NAME').count().sort_values('STATUS',ascending=False)['STATUS'][0:10]
+    else:
+        certified = f.loc[f['CASE_STATUS']=='CERTIFIED']
+        top = certified.groupby('SOC_NAME').count().sort_values('CASE_STATUS',ascending=False)['CASE_STATUS'][0:10]
     top_name = list(top.index)
     top_occ_num = list(top)
-    top_occ_num.append(len(f.loc[f['STATUS']=='CERTIFIED'])-sum(top_occ_num))
+    top_occ_num.append(len(certified)-sum(top_occ_num))
     top_name.append('Others')
     print(labels)
     return top_name,top_occ_num
@@ -17,10 +22,15 @@ def getocc(name):
 def getstate(name):
     f = pd.read_csv(name,sep=';',index_col=0,header=0)
     labels = list(f.columns)
-    top = f.loc[f['STATUS']=='CERTIFIED'].groupby('LCA_CASE_WORKLOC1_STATE').count().sort_values('STATUS',ascending=False)['STATUS'][0:10]
+    if '14' in name:
+        certified = f.loc[f['STATUS'] == 'CERTIFIED']
+        top = certified.groupby('LCA_CASE_WORKLOC1_STATE').count().sort_values('STATUS',ascending=False)['STATUS'][0:10]
+    else:
+        certified = f.loc[f['CASE_STATUS'] == 'CERTIFIED']
+        top = certified.groupby('WORKSITE_STATE').count().sort_values('CASE_STATUS',ascending=False)['CASE_STATUS'][0:10]
     top_name = list(top.index)
     top_state_num = list(top)
-    top_state_num.append(len(f.loc[f['STATUS']=='CERTIFIED'])-sum(top_state_num))
+    top_state_num.append(len(certified)-sum(top_state_num))
     top_name.append('Others')
     print(labels)
     return top_name,top_state_num
@@ -39,5 +49,5 @@ def pieplot(name,flag):
     plt.legend(top_name, loc=1)
     plt.show()
 
-pieplot('H1B_FY_2014.csv',2)
+pieplot('H1B_FY_2016.csv',2)
 # getdata('H1B_FY_2014.csv')
